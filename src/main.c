8381a5c89ef3654ff113e698d7cb0717b13be7fa
michalkolos <cool.kolos@gmail.com>
2020-04-18 10:31:37 +0100

    Added parsing of raw data to channel values (not working, possible hardware issues).

--- src/main.c
+++ src/main.c
@@ -16,6 +16,7 @@
 
     int serialBegin();
     int serialRead(uint8_t* frame);
+    int sbusParse(uint8_t* frame, uint16_t* channels);
 
     static const uint32_t _sbusBaud = 100000;
     static const uint8_t _numChannels = 16;
@@ -27,6 +28,7 @@
     static const uint8_t _payloadSize = 24;
     static const uint8_t _sbusLostFrame = 0x04;
     static const uint8_t _sbusFailSafe = 0x08;
+    static const char _serialPath[] = "/dev/AMA0";
 
     int serial_port = -1;
 
@@ -47,81 +49,19 @@ int main(int argc, char const *argv[]){
             printf("%3d ", frame[i]);
         }
         printf("\n");
+
+        // sbusParse(frame, channels);
+
+        // for(int i = 0; i < _numChannels; i++){
+        //     printf("%4d ", channels[i]);
+        // }
+        // printf("\n");
     }
 
 
 
 
-    // // Create new termios struc, we call it 'tty' for convention
-    // struct termios tty;
-    // memset(&tty, 0, sizeof (tty));
-
-    // // Read in existing settings, and handle any error
-    // if(tcgetattr(serial_port, &tty) != 0) {
-    //     printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
-    // }
-
-
-    // // Modify configuration
-    // tty.c_cflag |= PARENB;          // Set parity bit, enabling parity
-    // tty.c_cflag &= ~CSTOPB;         // Clear stop field, only one stop bit used in communication
-    // tty.c_cflag |= CS8;             // 8 bits per byte
-    // // tty.c_cflag &= ~CRTSCTS;        // Disable RTS/CTS hardware flow control
-    // tty.c_cflag |= CREAD | CLOCAL;  // Turn on READ & ignore ctrl lines (CLOCAL = 1)
-    // tty.c_lflag &= ~ICANON;         // Disable canonical mode
-    // tty.c_lflag &= ~ECHO;           // Disable echo
-    // tty.c_lflag &= ~ISIG;           // Disable interpretation of INTR, QUIT and SUSP
-
-    // tty.c_iflag &= ~(IXON | IXOFF | IXANY);         // Turn off s/w flow ctrl
     
-    // // Disable any special handling of received bytes
-    // tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); 
-
-    // // Prevent special interpretation of output bytes (e.g. newline chars)
-    // tty.c_oflag &= ~OPOST; 
-
-    // tty.c_oflag &= ~ONLCR;          // Prevent conversion of newline to carriage return/line feed
-    
-    // // tty.c_oflag &= ~OXTABS; // Prevent conversion of tabs to spaces (NOT PRESENT IN LINUX)
-    // // tty.c_oflag &= ~ONOEOT; // Prevent removal of C-d chars (0x004) in output (NOT PRESENT IN LINUX)
-
-    // tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
-    // tty.c_cc[VMIN] = 0;
-
-    // // Specifying a custom baud rate when using GNU C
-    // cfsetispeed(&tty, 100000);
-    // cfsetospeed(&tty, 100000);
-
-    // // Save tty settings, also checking for error
-    // if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
-    //     printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
-    // }
-
-    // // Allocate memory for read buffer, set size according to your needs
-    // char read_buf [256];
-    // memset(&read_buf, '\0', sizeof(read_buf));
-
-    // int channels[32];
-    // memset(&channels, '\0', sizeof(channels) * 4);
-    
-    
-
-    // // Read bytes. The behaviour of read() (e.g. does it block?,
-    // // how long does it block for?) depends on the configuration
-    // // settings above, specifically VMIN and VTIME
-    
-
-    // printf("Start reading. \n");
-
-
-    // while(1){
-        // int n = read(serial_port, &read_buf, sizeof(read_buf));
-
-        // printf("read\n");
-
-        // n is the number of bytes read. n may be 0 if no bytes were received, and can also be negative to signal an error.
-
-        // if(n >= 0){
 
         //     // switch (read_buf[0])
         //     // {
@@ -165,22 +105,7 @@ int main(int argc, char const *argv[]){
 		// 	channels[15] = (int) ((read_buf[20]>>5 | read_buf[21]<<3)                     & 0x07FF);
 
 
-        //     for(int i = 0; i < strlen(read_buf); i++){
-        //         printf("%2d/%3d ", i,  (int)read_buf[i]);
-        //     }
 
-        //     for(int i = 0; i < 16; i++){
-        //         printf("%2d/%4X ", i,  channels[i]);
-        //     }
-
-
-        //     printf("\n");
-
-        //     // printf("%s\n", read_buf);
-        // }
-
-        // printf("%s\n", read_buf);
-    // }
 
     close(serial_port);
 
@@ -190,9 +115,33 @@ int main(int argc, char const *argv[]){
 }
 
 
+int sbusParse(uint8_t* frame, uint16_t* channels){
+
+    	channels[0]  = (uint16_t) ((frame[0]     | frame[1] <<8)                     & 0x07FF);
+        channels[1]  = (uint16_t) ((frame[1]>>3  | frame[2] <<5)                     & 0x07FF);
+        channels[2]  = (uint16_t) ((frame[2]>>6  | frame[3] <<2 | frame[4]<<10)      & 0x07FF);
+        channels[3]  = (uint16_t) ((frame[4]>>1  | frame[5] <<7)                     & 0x07FF);
+        channels[4]  = (uint16_t) ((frame[5]>>4  | frame[6] <<4)                     & 0x07FF);
+        channels[5]  = (uint16_t) ((frame[6]>>7  | frame[7] <<1 | frame[8]<<9)       & 0x07FF);
+        channels[6]  = (uint16_t) ((frame[8]>>2  | frame[9] <<6)                     & 0x07FF);
+        channels[7]  = (uint16_t) ((frame[9]>>5  | frame[10]<<3)                     & 0x07FF);
+        channels[8]  = (uint16_t) ((frame[11]    | frame[12]<<8)                     & 0x07FF);
+        channels[9]  = (uint16_t) ((frame[12]>>3 | frame[13]<<5)                     & 0x07FF);
+        channels[10] = (uint16_t) ((frame[13]>>6 | frame[14]<<2 | frame[15]<<10)     & 0x07FF);
+        channels[11] = (uint16_t) ((frame[15]>>1 | frame[16]<<7)                     & 0x07FF);
+        channels[12] = (uint16_t) ((frame[16]>>4 | frame[17]<<4)                     & 0x07FF);
+        channels[13] = (uint16_t) ((frame[17]>>7 | frame[18]<<1 | frame[19]<<9)      & 0x07FF);
+        channels[14] = (uint16_t) ((frame[19]>>2 | frame[20]<<6)                     & 0x07FF);
+        channels[15] = (uint16_t) ((frame[20]>>5 | frame[21]<<3)                     & 0x07FF);
+
+
+    return 0;
+}
+
+
 int serialBegin(){
 
-    serial_port = open("/dev/ttyS0", O_RDWR | O_NOCTTY);
+    serial_port = open(_serialPath, O_RDWR | O_NOCTTY);
 
     // Check for errors
     if (serial_port < 0) {
@@ -248,41 +197,7 @@ int serialRead(uint8_t* frame){
         prevByte = byteBuffer[0];
     }
 
-    // // reset the parser state if too much time has passed
-    //     static elapsedMicros _sbusTime = 0;
-    //     if (_sbusTime > SBUS_TIMEOUT_US) {_parserState = 0;}
-    //     // see if serial data is available
-    //     while (_bus->available() > 0) {
-    //         _sbusTime = 0;
-    //         _curByte = _bus->read();
-    //         // find the header
-    //         if (_parserState == 0) {
-    //                 if ((_curByte == _sbusHeader) && ((_prevByte == _sbusFooter) || ((_prevByte & _sbus2Mask) == _sbus2Footer))) {
-    //                     _parserState++;
-    //                 } else {
-    //                     _parserState = 0;
-    //                 }
-    //         } else {
-    //             // strip off the data
-    //             if ((_parserState-1) < _payloadSize) {
-    //                 _payload[_parserState-1] = _curByte;
-    //                 _parserState++;
-    //             }
-    //             // check the end byte
-    //             if ((_parserState-1) == _payloadSize) {
-    //                 if ((_curByte == _sbusFooter) || ((_curByte & _sbus2Mask) == _sbus2Footer)) {
-    //                     _parserState = 0;
-    //                     return 0;
-    //                 } else {
-    //                     _parserState = 0;
-    //                     return -1;
-    //                 }
-    //             }
-    //         }
-    //         _prevByte = _curByte;
-    //     }
-    //     // return false if a partial packet
-    //     return -1;
+
 
     return 0;
 }
